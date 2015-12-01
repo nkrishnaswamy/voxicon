edf4e9f552253534b5349b05c1cd88d7523ebcc6
nkrishnaswamy <nkrishna@brandeis.edu>
2015-11-30 21:42:57 -0500

    Adding animation testbed scene

---
+++ Voxicon/Assets/Scripts/GraspScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraspScript : MonoBehaviour {
+	private Animator anim;
+	private int grasper;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			grasper = 1;
+		} else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			grasper = 2;
+		} else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			grasper = 3;
+		} else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			grasper = 4;
+		} else if (Input.GetKeyDown (KeyCode.Space)) {
+			grasper = 0;
+		}
+		anim.SetInteger ("grasp", grasper);
+		Debug.Log (grasper);
+	}
+}
