e3cd5de3337e1da3fe4549d529a212dcbf39b22d
harkan94 <harkan@brandeis.edu>
2016-03-17 18:33:17 -0400

    FreeCamera

--- Voxicon/Assets/Scripts/GhostFreeRoamCamera.cs
+++ Voxicon/Assets/Scripts/GhostFreeRoamCamera.cs
@@ -23,6 +23,9 @@ public class GhostFreeRoamCamera : MonoBehaviour
 	private bool togglePressed = false;
 	
 	private Rigidbody rb;
+	private Vector3 deltaPosition;
+
+	private float angle = 0;
 
 	private void OnEnable()
 	{
@@ -33,22 +36,22 @@ public class GhostFreeRoamCamera : MonoBehaviour
 		}
 	}
 	
-	private void Update()
+	private void FixedUpdate()
 	{
-			if (allowMovement)
+		if (allowMovement)
 		{
 			bool lastMoving = moving;
-			Vector3 deltaPosition = Vector3.zero;
+			deltaPosition = Vector3.zero;
 			
 			if (moving)
 				currentSpeed += increaseSpeed * Time.deltaTime;
 			
 			moving = false;
 			
-			CheckMove(forwardButton, ref deltaPosition, transform.forward);
-			CheckMove(backwardButton, ref deltaPosition, -transform.forward);
-			CheckMove(rightButton, ref deltaPosition, transform.right);
-			CheckMove(leftButton, ref deltaPosition, -transform.right);
+			CheckMove(forwardButton, transform.forward);
+			CheckMove(backwardButton, -transform.forward);
+			CheckMove(rightButton, transform.right);
+			CheckMove(leftButton, -transform.right);
 			
 			if (moving)
 			{
@@ -93,7 +96,7 @@ public class GhostFreeRoamCamera : MonoBehaviour
 		rb = GetComponent<Rigidbody> ();
 	}
 
-	private void CheckMove(KeyCode keyCode, ref Vector3 deltaPosition, Vector3 directionVector)
+	private void CheckMove(KeyCode keyCode, Vector3 directionVector)
 	{
 		if (Input.GetKey(keyCode))
 		{
@@ -101,39 +104,56 @@ public class GhostFreeRoamCamera : MonoBehaviour
 			deltaPosition += directionVector;
 		}
 	}
-	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Boundary") {
-			currentSpeed = 0;
-//			Debug.Log(other.gameObject.name);
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag != "Boundary") {
+////			currentSpeed = 0;
+//			Debug.Log(other.transform.name);
+//			Vector3 dir = other.transform.position - transform.position;
+//			dir = other.transform.InverseTransformDirection(dir);
+////			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+//			Debug.Log(dir);
+//			Debug.Log (deltaPosition);
+//			deltaPosition -= dir;
+//			transform.position += deltaPosition * currentSpeed * Time.deltaTime;
+			Physics.IgnoreCollision (GetComponent<Collider> (), other.gameObject.GetComponent<Collider> ());
+//			transform.position -= dir;
+//			transform.position = deltaPosition * currentSpeed * Mathf.Sin ((Vector3.Angle(deltaPosition, other.transform.local)));
 //			if (transform.position.x == other.transform.position.x) {
-//				Debug.Log("x " + other.gameObject.name);
+//				Debug.Log("1");
 //			}
 //			if (transform.position.y == other.transform.position.y) {
-//				Debug.Log ("y " + other.gameObject.name);
+//				Debug.Log ("2");
 //			}
 //			if (transform.position.z == other.transform.position.z) {
-//				Debug.Log("z " + other.gameObject.name);
+//				Debug.Log("3");
 //			}
-//			rb.AddForce(other.transform.up);
+//			transform.position -= deltaPosition * currentSpeed * Time.deltaTime;
 		}
 	}
 	
-	void OnTriggerStay(Collider other) {
-		if (other.tag == "Boundary") {
-			currentSpeed = 0;
-//			Debug.Log(other.gameObject.name);
+	void OnCollisionStay(Collision other) {
+		if (other.gameObject.tag != "Boundary") {
+//			currentSpeed = 0;
+			Physics.IgnoreCollision(GetComponent<Collider>(), other.gameObject.GetComponent<Collider>());
 //			if (transform.position.x == other.transform.position.x) {
-//				Debug.Log("x " + other.gameObject.name);
+//				Debug.Log("x " + other.name);
 //			}
 //			if (transform.position.y == other.transform.position.y) {
-//				Debug.Log ("y " + other.gameObject.name);
+//				Debug.Log ("y " + other.name);
 //			}
 //			if (transform.position.z == other.transform.position.z) {
-//				Debug.Log("z " + other.gameObject.name);
+//				Debug.Log("z " + other.name);
 //			}
-		}
+//			transform.position -= deltaPosition * currentSpeed * Time.deltaTime;
+//		}
 //		rb.AddForce(other.transform.up);
 //		Debug.Log(other.gameObject.name);
+//			Vector3 dir = other.transform.position - transform.position;
+//			dir = other.transform.InverseTransformDirection(dir);
+//			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+//			Debug.Log(angle);
+		}
+
 	}
 
 
