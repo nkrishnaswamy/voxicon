6bbc3182a97c2a7c71982647d7f76cf01f400528
Keigh Rim <keigh.rim@gmail.com>
2017-02-23 14:41:59 -0500

    added build pipeline for Windows os

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -9,13 +9,13 @@ public class Builder : Editor {
 	public string buildName = "VoxSimTrials";
 	List<string> scenes = new List<string>(){"Assets/Scenes/VoxSimMenu.unity"};
 
-	public override void OnInspectorGUI () 
+	public override void OnInspectorGUI ()
 	{
 		base.OnInspectorGUI();
 
 		if (GUILayout.Button ("Build", GUILayout.Height (30))) {
-			using (System.IO.StreamWriter file = 
-			       new System.IO.StreamWriter(@"Assets/Resources/ScenesList.txt"))
+			using (System.IO.StreamWriter file =
+				new System.IO.StreamWriter(@"Assets/Resources/ScenesList.txt"))
 			{
 				string scenesDirPath = Application.dataPath + "/Scenes/";
 				string [] fileEntries = Directory.GetFiles(Application.dataPath+"/Scenes/","*.unity");
@@ -28,6 +28,7 @@ public class Builder : Editor {
 				}
 			}
 			BuildPipeline.BuildPlayer(scenes.ToArray(),"Build/mac/"+buildName,BuildTarget.StandaloneOSXUniversal,BuildOptions.None);
+            BuildPipeline.BuildPlayer(scenes.ToArray(),"Build/win/"+buildName,BuildTarget.StandaloneWindows,BuildOptions.None);
 			//BuildPipeline.BuildPlayer(scenes.ToArray(),"Build/web/"+buildName,BuildTarget.WebPlayer,BuildOptions.None);
 		}
 	}
